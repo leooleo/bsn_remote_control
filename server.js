c5687d39d3a84a3d9bc8b6c34442653abb3aa646
leooleo <leo.ms097@gmail.com>
2019-10-29 18:42:33 -0300

    Fully integrated!

--- server.js
+++ server.js
@@ -2,35 +2,55 @@ const express = require('express')
 const port = process.env.port || 3000
 const serverComunication = require('./src/serverComunication')
 const { spawn } = require('child_process');
+const sleep = require('system-sleep')
 const app = express()
 
 bsnRunning = false
 
-app.get('/', function (req, res) {    
+commands = [
+    ['configurations/knowledge_repository/', 'roslaunch data_access.launch'],
+    ['configurations/system_manager/', 'roslaunch enactor.launch'],
+    ['configurations/logging_infrastructure/', 'roslaunch logger.launch'],
+    ['configurations/target_system/', 'roslaunch probe.launch'],
+    ['configurations/target_system/', 'roslaunch effector.launch'],
+    ['configurations/target_system/', 'roslaunch g4t1.launch'],
+    ['configurations/target_system/', 'roslaunch g3t1_1.launch'],
+    ['configurations/target_system/', 'roslaunch g3t1_2.launch'],
+    ['configurations/target_system/', 'roslaunch g3t1_3.launch'],
+    ['configurations/simulation/', 'roslaunch injector.launch'],
+    ['configurations/system_manager/', 'roslaunch engine.launch'],
+]
+
+app.get('/', function (req, res) {
     res.send('ok');
 });
 
-app.get('/start', function (req, res) {    
-    if(childs.length >= 1) {
+app.get('/start', function (req, res) {
+    if (bsnRunning) {
         res.send('already running')
     }
     else {
-        var ls = spawn('bash', ['../run.sh']);
-        childs.push(ls)
-        console.log(childs.length)
+        spawn('gnome-terminal', ['-x', 'roscore']);
+        sleep(5000)
+        for (var i = 0; i < commands.length; i++) {
+
+            spawn('gnome-terminal', ['--working-directory=catkin_ws/src/bsn_ros/' + commands[i][0], '-e', commands[i][1]])
+            sleep(1000)
+        }
         res.send('started');
+        bsnRunning = true
     }
-    
+
 });
 
-app.get('/stop', function (req, res) {   
-    if(bsnRunning == false) {
-        res.send('bsn is not running');    
-    } 
+app.get('/stop', function (req, res) {
+    if (bsnRunning == false) {
+        res.send('bsn is not running');
+    }
     else {
-        var ls = await spawn('pkill', ['-f', 'ros']);
-        ls.
+        var ls = spawn('pkill', ['-f', 'ros']);
         res.send('stopped');
+        bsnRunning = false
     }
 });
 
